618a5df90be1279d492571807d9a9a85e53819d6
Leon <hualiang.tian@gmail.com>
2021-06-14 21:14:07 -0400

    added helper method and private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,19 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @owner ||= User.find_by(id: session[:user_id])
+    end
+
+    private
+      def redirect_if_not_logged_in
+        if !logged_in?
+          redirect to "/login"
+        end
+      end
 end
