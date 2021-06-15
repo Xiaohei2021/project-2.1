ef1ff54570396b09ced0a1bf376ba0a438e24d4b
Leon <hualiang.tian@gmail.com>
2021-06-14 20:40:37 -0400

    added failure route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,5 +11,19 @@ class UserController < ApplicationController
     get "/login" do
         erb :'users/login'
     end
-     
+
+    post "/login" do
+        user = User.find_by(username: params[:username])
+
+    end
+
+     get "/failure" do
+        erb :'/users/failure'
+     end
+
+     get "/account/:id" do
+        @user = User.find(params[:id])
+        erb :'/users/account'
+     end
+
 end
