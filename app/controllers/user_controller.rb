59425af88e481018c73df5172f0842e4125d5e8e
Leon <hualiang.tian@gmail.com>
2021-06-14 23:58:25 -0400

    final push

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,22 +25,21 @@ class UserController < ApplicationController
 
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect to "/getaways"
+            redirect to "/account/#{user.id}"
         else
             redirect to "/failure"
         end
-
     end
 
      get "/failure" do
         erb :'/users/failure'
      end
 
-    #  get "/account/:id" do
-    #     redirect_if_not_logged_in
-    #     @user = User.find(params[:id])
-    #     erb :'/users/account'
-    #  end
+     get "/account/:id" do
+        
+        @user = User.find(params[:id])
+        erb :'/users/account'
+     end
 
      get "/logout" do
         session.clear
