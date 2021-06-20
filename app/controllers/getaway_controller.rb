74d1b74bf70729a718c6f4ab0049649e363a3910
Leon <hualiang.tian@gmail.com>
2021-06-19 22:20:59 -0400

    final project, no more css tinkering, need to focus on preparing for the assessment

--- app/controllers/getaway_controller.rb
+++ app/controllers/getaway_controller.rb
@@ -17,16 +17,14 @@ class GetawayController < ApplicationController
       end
 
     get '/getaways/:id/edit' do 
-       
         redirect_if_not_logged_in
         @lalaland = Getaway.find_by_id(params[:id])
-        redirect_if_not_authorized
+        redirect_if_not_authorized2
         erb :'lalaland/edit'
     end
       
     post '/getaways' do
       redirect_if_not_logged_in
-
       # @lalaland = Getaway.create(:destination => params[:destination], :advantures => params[:advantures],:duration_of_stay => params[:duration_of_stay], :season_to_visit => params[:season_to_visit], :location => params[:location], :saving_required => params[:saving_required])
       @lalaland = Getaway.new(params)
       # binding.pry
@@ -39,7 +37,7 @@ class GetawayController < ApplicationController
       redirect_if_not_logged_in
       @lalaland = Getaway.find(params[:id])
       # binding.pry
-      redirect_if_not_authorized
+      redirect_if_not_authorized2
       @lalaland.update(params["lalaland"])
       redirect :"/getaways/#{@lalaland.id}"
      
@@ -48,21 +46,31 @@ class GetawayController < ApplicationController
     delete '/getaways/:id' do 
       redirect_if_not_logged_in
       @lalaland = Getaway.find_by_id(params[:id])
-      redirect_if_not_authorized
+      redirect_if_not_authorized1
       @lalaland.destroy
       redirect to '/getaways'
     end
   
-    get '/intruding' do
+    get '/intruding1' do
       erb :'lalaland/no_touchy'
+    end
+
+    get '/intruding2' do
+      erb :'lalaland/no_touchy_edit'
     end
 
 
     private
-      def redirect_if_not_authorized
+      def redirect_if_not_authorized1
           if @lalaland.user != current_user
-            redirect to "/intruding"
+            redirect to "/intruding1"
           end
       end
+
+      def redirect_if_not_authorized2
+        if @lalaland.user != current_user
+          redirect to "/intruding2"
+        end
+    end
 
 end
